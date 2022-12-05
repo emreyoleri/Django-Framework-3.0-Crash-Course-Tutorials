363064a9907f9678d2c78c8fca8c48235da34080
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-06 01:21:01 +0300

    Bootstrap styling and templated updated

--- accounts/views.py
+++ accounts/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 def home(request):
     return render(request, "accounts/dashboard.html")
 
-
 def products(request):
-    return HttpResponse("products")
+    return render(request, "accounts/products.html")
+
 
 def customer(request):
-    return HttpResponse("customer")
+    return render(request, "accounts/customer.html")
