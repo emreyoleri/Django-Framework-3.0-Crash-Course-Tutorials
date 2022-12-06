4c69e2ae7e72985f3d6dc6075321dbc70545f201
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-06 18:21:00 +0300

    Product and Order model created

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,33 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ("Indoor", "Indoor"),
+        ("Out Door", "Out Door"),
+    )
+    name = models.CharField(max_length=255, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(
+        max_length=255, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Order(models.Model):
+
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for delivery"),
+        ("Delivered", "Delivered"),
+    )
+    # customer =
+    # product =
+
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=255, null=True, choices=STATUS)
