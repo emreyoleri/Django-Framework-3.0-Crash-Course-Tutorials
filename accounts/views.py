847666333fdb936c741da50f9e8ed9d3be97f860
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-08 16:27:36 +0300

    create user form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import *
 from .forms import OrderForm
 from .filters import OrderFilter
@@ -15,7 +17,17 @@ def login(request):
 
 def register(request):
 
-    context = {}
+    form = UserCreationForm()
+
+    if request.method == "POST":
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+    context = {"form": form}
 
     return render(request, "accounts/register.html", context)
 
