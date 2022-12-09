97de82914e270461bd99d3cd225e1a103c65596a
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-10 00:17:50 +0300

    image upload form

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("user/", views.userPage, name="user-page"),
+    path('account/', views.accountSettings, name="account"),
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
     path("register/", views.registerPage, name="register"),
--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.contrib import messages
 
 from .decorators import unauthenticated_user, allowed_users, admin_only
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 
 
@@ -127,6 +127,27 @@ def userPage(request):
     }
 
     return render(request, "accounts/user.html", context)
+
+
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["customer"])
+def accountSettings(request):
+
+    customer = request.user.customer
+
+    form = CustomerForm(instance=customer)
+
+    if request.method == "POST":
+
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+
+        if form.is_valid():
+
+            form.save()
+
+    context = {"form": form}
+
+    return render(request, "accounts/account_settings.html", context)
 
 
 @login_required(login_url="login")
