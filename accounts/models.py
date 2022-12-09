8df2b778532f0859a03afd1ad5d9dc2222b10b1e
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-09 22:56:03 +0300

    Add field image

--- accounts/models.py
+++ accounts/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User , null=True , on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=True)
     phone = models.CharField(max_length=255, null=True)
     email = models.CharField(max_length=255, null=True)
+    profile_pic = models.ImageField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
