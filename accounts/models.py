1c7755fda3140a8e01b6e42d5bbe2370491f1819
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-10 00:39:35 +0300

    Image File Upload to User Profile Model

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=True)
     phone = models.CharField(max_length=255, null=True)
     email = models.CharField(max_length=255, null=True)
-    profile_pic = models.ImageField(null=True, blank=True)
+    profile_pic = models.ImageField(
+        default="user-logo1.jpg", null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
