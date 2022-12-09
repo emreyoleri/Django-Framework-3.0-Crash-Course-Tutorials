3c5dc6551ebb845b9a27a6e1e983b504b375044f
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-09 22:51:09 +0300

    User Profile With One To One Relationship

--- accounts/views.py
+++ accounts/views.py
@@ -34,6 +34,8 @@ def registerPage(request):
 
             user.groups.add(group)
 
+            Customer.objects.create(user=user)
+
             messages.success(
                 request, "Account was created for " + username)
 
@@ -105,8 +107,25 @@ def home(request):
     return render(request, "accounts/dashboard.html", context)
 
 
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["customer"])
 def userPage(request):
-    context = {}
+
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+
+    delivered = orders.filter(status="Delivered").count()
+
+    pending = orders.filter(status="Pending").count()
+
+    context = {
+        "orders": orders,
+        "total_orders": total_orders,
+        "delivered": delivered,
+        "pending": pending
+    }
+
     return render(request, "accounts/user.html", context)
 
 
