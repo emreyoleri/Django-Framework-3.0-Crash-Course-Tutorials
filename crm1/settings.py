6531c64b03bc1729e8b323579d55e99a702849c4
Emre Yoleri <emreyoleri01@gmail.com>
2022-12-11 00:01:45 +0300

    conntect to local postgres

--- crm1/settings.py
+++ crm1/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "crm1.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Demo_Test",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "POST":  "54324"
     }
 }
 
